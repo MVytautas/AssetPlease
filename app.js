51c6b71a2e5da6b46802872161494a06d4c67cb1
Vytautas <vmikalainis@gmail.com>
2019-03-24 11:07:43 +0100

    issuer form added

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-const express = require('express')
-const app = express()
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+var express = require('express')
+, app = express()
+, server = require('http').createServer(app)
+, io = require('/home/.../.npm/socket.io')
+, npid = require("npid")
+, uuid = require('node-uuid')
+, Room = require('./room.js')
+, _ = require('underscore')._;
+
+
+app.set('port', 3000);
+app.set('ipaddr', "127.0.0.1");
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(__dirname + '/public'));
+app.use('/components', express.static(__dirname + '/components'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/icons', express.static(__dirname + '/icons'));
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+
+/* Store process-id (as priviledged user) */
+try {
+    npid.create('/var/run/advanced-chat.pid', true);
+} catch (err) {
+    console.log(err);
+    //process.exit(1);
+}
